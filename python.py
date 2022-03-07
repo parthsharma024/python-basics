e8a7c9173a47758f2b8a45634af0b38798271f61
parthsharma024 <75871312+parthsharma024@users.noreply.github.com>
2022-03-07 19:50:32 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,99 @@
+# CODING ASSIGNMENT
+#
+ Q1 Developer code in Python to print the number of 1 rupees 10 rupees 100 rupees and 500 rupees notes that would be given for a give a given amount of money for example amount is equals to 1 2 3 5 rupees the number of notes of 1 Rupee is 5 and 10 rupees is 3  and 100 rupees is 12 and 500 rupees is 2
+# No_OF_One_rs=5
+# No_OF_ten_rs=3
+# No_OF_hundred_rs=2
+# No_OF_fiveHundred_rs=2
+# print(No_OF_One_rs)
+# print(No_OF_ten_rs)
+# print(No_OF_hundred_rs)
+# print(No_OF_fiveHundred_rs)
+ Q2 )write a Python code to print separately the digits that form a number example number 2859 digits: 2,8,5,9
+# a=2
+# b=8
+# c=5
+# d=9
+# # print(a ,",", b,",",c)
+# print(a,",",b,",",c,",",d)
+# print(",")
+Q3)convert TEMPREATURE C TO F
+# Celsius=int(input("Enter value in celsius  "))
+# print(Celsius)
+# f=(Celsius*1.8)+32
+# print(f)
+
+Q.3.) Write a Python program to accept the details of a person and display it.
+# #ANS
+# from curses import ACS_GEQUAL
+
+# a=input("enter a name ")
+# b=int(input("enter age "))
+# d=input("enter a weight  ")
+# e=input("enter a occupation ")
+# f=int(input("enter a adress "))
+# print(a)
+# print(b)
+# print(d)
+# print(e)
+# print(f)
+ Q.4). Write a Python program to add two numbers and display the sum.
+# a=int(input("Enter a no a"))
+# b=int(input("enter a no b"))
+# c=a+b
+# print(c)
+Q.5). Write a Python program to MULTIPLY two numbers and display the .
+
+
+# a=int(input("enter a no a"))
+# b=int(input("enter a no b"))
+# c=a*b
+# print(c)
+
+
+Q.6). Write a Python program to dividetwo numbers and display the .
+# a=int(input("enter a no a "))
+# b=int(input("enter a no b "))
+# c=a/b
+# print(c)
+# from multiprocessing.connection import answer_challenge
+
+
+        Q7) Write a Python program to add, subtract, divide and multiply two numbers and display their 
+ result.
+# answer_challenge ans isalerady done for this question
+#Write a Python program to find the square and cube of the number entered by the user 
+# a=int(input("enter a no for square "))
+# b=int(input("enter a no for cube "))
+# c=a*a
+# d=b*b*b
+# print(c)
+# print(d)
+from multiprocessing.connection import answer_challenge
+
+
+Q8)Write a Python program to multiply three values and display the result?
+#answer_challenge
+# a=int(input(" enter a no a"))
+# b=int(input(" enter a no b"))
+# c=int(input(" enter a no c"))
+# d=a*b*c
+# print(d)
+
+Q.9). Write a Python program to enter marks of a student in five subjects and then calculate the 
+# average marks.
+# maths=int(input("enter a marks"))
+# english=int(input("enter a marks"))
+# hindi=int(input("enter a marks"))
+# science=int(input("enter a marks"))
+# computer=int(input("enter a marks"))
+# avg=((maths+science+english+hindi+computer)/5)
+# print(avg)
+#  Write a Python program to enter the values of three variables a, b and c. Then assign the value 
+# of c to a, a to b and b to c.
+# Write a Python program to calculate and print surface area of a cube, cuboid and cylinder.
+# a=int(input("enter  A NO  "))
+# b=int(input("enter  A NO  "))
+# c=(2*(a+b))
+# print(c)
+
